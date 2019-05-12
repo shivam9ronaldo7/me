c523eed0321847c9f9b475459dceafaf096f5164
shivam9ronaldo7 <shivam9ronaldo7@gmail.com>
2019-05-12 09:04:31 +0530

    Updates

---
+++ precache-manifest.4b0f4f0104a23e8f9e8bbe9989754fc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe94b1e1486a618fc6bd219a4310c8d",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "cc992fdaaf81918c2246",
+    "url": "/me/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/me/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "cc992fdaaf81918c2246",
+    "url": "/me/static/js/main.c4d1ada4.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  }
+]);
